9b347e99529342d076c5524b1cef01652a44a27d
kirimaru <nainaistar@gmail.com>
2021-06-06 06:08:10 +0000

    [Add] ソースコードも入れてみる

--- pages/index.js
+++ pages/index.js
@@ -74,14 +74,27 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    
+        <a
+          href="https://twitter.com/nainaistar"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{''}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a> */}
+          連絡先：きり丸
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> */}
+        </a>
+
+      　　　　　　　
+     
+        <a 
+          href="https://github.com/hirotoKirimaru/IamEternal17"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ソースコード
+          
+        </a>
+      
       </footer>
     </div>
   )
