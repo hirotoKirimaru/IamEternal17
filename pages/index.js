d01bb26daed94453571d9d930ca40e305dfdd661
kirimaru <nainaistar@gmail.com>
2021-06-06 05:11:51 +0000

    [Add] イメージ

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,13 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {createBirthDom()}
+
+          {/* Get started by editing{' '} */}
+          {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
-        <div className={styles.grid}>
+        {/* <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -47,19 +49,39 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
       <footer className={styles.footer}>
-        <a
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{''}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        </a> */}
       </footer>
     </div>
   )
+}
+
+function createBirthDom(){
+
+  const now = getNowDateWithString()
+  return <>
+  <p>現在時刻：{now}</p>
+  <p>生年月日：{now}</p>
+  <p>年齢：17才Xヵ月</p>
+  </>
+}
+
+
+function getNowDateWithString(){
+var dt = new Date();
+var y = dt.getFullYear();
+var m = ("00" + (dt.getMonth()+1)).slice(-2);
+var d = ("00" + dt.getDate()).slice(-2);
+var result = y + "/" + m + "/" + d;
+return result;
 }
