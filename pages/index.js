6f0e6542a1a8eebbfac4c2c5dfe69be331420059
kirimaru <nainaistar@gmail.com>
2021-06-06 04:35:42 +0000

    [Add] 不要？

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          私は永遠の17歳
         </h1>
 
         <p className={styles.description}>
